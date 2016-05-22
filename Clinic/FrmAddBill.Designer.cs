475546fd0438d486251f1dd36093429980216c20
MangTomas23 <matos.adrianpaul@gmail.com>
2016-05-23 02:10:31 +0800

    Update layout

--- Clinic/FrmAddBill.Designer.cs
+++ Clinic/FrmAddBill.Designer.cs
@@ -28,13 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dtpDate = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.dv1 = new System.Windows.Forms.DataGridView();
-            this.colItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -45,6 +43,8 @@
             this.btnManage = new DevExpress.XtraEditors.SimpleButton();
             this.txtAmount = new DevExpress.XtraEditors.TextEdit();
             this.cbItem = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.colItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dv1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtAmount.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cbItem.Properties)).BeginInit();
@@ -53,7 +53,7 @@
             // dtpDate
             // 
             this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpDate.Location = new System.Drawing.Point(583, 55);
+            this.dtpDate.Location = new System.Drawing.Point(583, 16);
             this.dtpDate.Name = "dtpDate";
             this.dtpDate.Size = new System.Drawing.Size(153, 20);
             this.dtpDate.TabIndex = 100;
@@ -61,7 +61,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(580, 39);
+            this.label1.Location = new System.Drawing.Point(552, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(30, 13);
             this.label1.TabIndex = 1;
@@ -69,39 +69,24 @@
             // 
             // dv1
             // 
+            this.dv1.AllowUserToAddRows = false;
+            this.dv1.AllowUserToDeleteRows = false;
             this.dv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colItem,
             this.colAmount});
-            this.dv1.Location = new System.Drawing.Point(12, 81);
+            this.dv1.Location = new System.Drawing.Point(12, 42);
             this.dv1.Name = "dv1";
-            this.dv1.Size = new System.Drawing.Size(724, 169);
+            this.dv1.RowHeadersVisible = false;
+            this.dv1.Size = new System.Drawing.Size(724, 201);
             this.dv1.TabIndex = 2;
             this.dv1.CellStateChanged += new System.Windows.Forms.DataGridViewCellStateChangedEventHandler(this.dv1_CellStateChanged);
             this.dv1.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dv1_RowStateChanged);
             // 
-            // colItem
-            // 
-            dataGridViewCellStyle3.NullValue = null;
-            this.colItem.DefaultCellStyle = dataGridViewCellStyle3;
-            this.colItem.HeaderText = "Item";
-            this.colItem.Name = "colItem";
-            this.colItem.Width = 500;
-            // 
-            // colAmount
-            // 
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle4.Format = "N2";
-            dataGridViewCellStyle4.NullValue = null;
-            this.colAmount.DefaultCellStyle = dataGridViewCellStyle4;
-            this.colAmount.HeaderText = "Amount";
-            this.colAmount.Name = "colAmount";
-            this.colAmount.Width = 180;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 292);
+            this.label2.Location = new System.Drawing.Point(4, 286);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(27, 13);
             this.label2.TabIndex = 3;
@@ -110,7 +95,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(506, 292);
+            this.label3.Location = new System.Drawing.Point(506, 286);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 5;
@@ -119,7 +104,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(515, 263);
+            this.label4.Location = new System.Drawing.Point(515, 252);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 13);
             this.label4.TabIndex = 7;
@@ -127,7 +112,7 @@
             // 
             // txtTotal
             // 
-            this.txtTotal.Location = new System.Drawing.Point(555, 260);
+            this.txtTotal.Location = new System.Drawing.Point(555, 249);
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
             this.txtTotal.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -136,27 +121,27 @@
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(687, 306);
+            this.btnAdd.Location = new System.Drawing.Point(687, 300);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(49, 23);
-            this.btnAdd.TabIndex = 2;
+            this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(252, 359);
+            this.btnSave.Location = new System.Drawing.Point(324, 336);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(119, 23);
             this.btnSave.TabIndex = 9;
-            this.btnSave.Text = "&Save to database";
+            this.btnSave.Text = "&Save";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnPrint
             // 
-            this.btnPrint.Location = new System.Drawing.Point(377, 359);
+            this.btnPrint.Location = new System.Drawing.Point(162, 12);
             this.btnPrint.Name = "btnPrint";
             this.btnPrint.Size = new System.Drawing.Size(119, 23);
             this.btnPrint.TabIndex = 9;
@@ -175,25 +160,43 @@
             // 
             // txtAmount
             // 
-            this.txtAmount.Location = new System.Drawing.Point(508, 309);
+            this.txtAmount.Location = new System.Drawing.Point(508, 303);
             this.txtAmount.Name = "txtAmount";
             this.txtAmount.Size = new System.Drawing.Size(173, 20);
-            this.txtAmount.TabIndex = 103;
+            this.txtAmount.TabIndex = 2;
             // 
             // cbItem
             // 
-            this.cbItem.Location = new System.Drawing.Point(7, 309);
+            this.cbItem.Location = new System.Drawing.Point(7, 303);
             this.cbItem.Name = "cbItem";
             this.cbItem.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
             this.cbItem.Size = new System.Drawing.Size(489, 20);
             this.cbItem.TabIndex = 1;
             // 
+            // colItem
+            // 
+            dataGridViewCellStyle1.NullValue = null;
+            this.colItem.DefaultCellStyle = dataGridViewCellStyle1;
+            this.colItem.HeaderText = "Item";
+            this.colItem.Name = "colItem";
+            this.colItem.Width = 540;
+            // 
+            // colAmount
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle2.Format = "N2";
+            dataGridViewCellStyle2.NullValue = null;
+            this.colAmount.DefaultCellStyle = dataGridViewCellStyle2;
+            this.colAmount.HeaderText = "Amount";
+            this.colAmount.Name = "colAmount";
+            this.colAmount.Width = 180;
+            // 
             // FrmAddBill
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(748, 394);
+            this.ClientSize = new System.Drawing.Size(748, 371);
             this.Controls.Add(this.cbItem);
             this.Controls.Add(this.txtAmount);
             this.Controls.Add(this.btnManage);
@@ -208,6 +211,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtpDate);
             this.Name = "FrmAddBill";
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FrmAddBill";
             ((System.ComponentModel.ISupportInitialize)(this.dv1)).EndInit();
@@ -230,10 +234,10 @@
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnPrint;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colItem;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colAmount;
         private DevExpress.XtraEditors.SimpleButton btnManage;
         private DevExpress.XtraEditors.TextEdit txtAmount;
         private DevExpress.XtraEditors.ComboBoxEdit cbItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAmount;
     }
 }
