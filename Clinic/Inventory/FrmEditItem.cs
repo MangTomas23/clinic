94fa6458633dc702d9e5f0bb55ff97270dc4bd3c
MangTomas23 <matos.adrianpaul@gmail.com>
2016-05-22 18:30:55 +0800

    Get set fields

--- Clinic/Inventory/FrmEditItem.cs
+++ Clinic/Inventory/FrmEditItem.cs
@@ -7,16 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace Clinic.Inventory
 {
     public partial class FrmEditItem : Form
     {
         private string id;
+        private IMongoCollection<BsonDocument> collection;
+        
         public FrmEditItem(string id)
         {
             InitializeComponent();
             this.id = id;
+            collection = MainForm.database.GetCollection<BsonDocument>("inventory");
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+            var result = collection.Find(filter).First();
+
+            txtItem.Text = result["item"].ToString();
+            txtQuantity.Text = result["quantity"].ToString();
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
